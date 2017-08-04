9a5081460ffd68e306d4d3a2d637d3370369e9e0
BartPu <bpuchalski@tlen.pl>
2017-08-04 13:18:10 +0200

    last commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,16 +12,6 @@ module.exports = function(grunt) {
                 }
             }
         },
-        imagemin: {
-            dynamic: {
-                files: [{
-                    expand: true,
-                    cwd: 'images/',
-                    src: ['**/*.{png,jpg,gif}'],
-                    dest: 'images/build/'
-                }]
-            }
-        },
         watch: {
             scripts: {
                 files: ['sass/*.sass'],
@@ -48,10 +38,9 @@ module.exports = function(grunt) {
     });
     // Load the plugins tasks
     grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync')
     // Default task(s).
 
-    grunt.registerTask('default', ['browserSync', 'sass', 'imagemin', 'watch']);
+    grunt.registerTask('default', ['browserSync', 'sass', 'watch']);
 };
