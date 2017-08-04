f82d7f4397560aa91b05c9cdd946347e4eba6e7d
BartPu <bpuchalski@tlen.pl>
2017-08-04 11:22:04 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var a = prompt('Enter a lenght');
+var h = prompt('Enter h lenght');
+var triangleArea = (a * h) / 2
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
